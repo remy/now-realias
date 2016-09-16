09846397453413abe22533af10510aff025ac09d
Remy Sharp <remy@remysharp.com>
2016-09-16 15:57:59 +0100

    fix: find the *last* alias
    Fixes issue if the previous release didn't have an alias, so the tool will keep searching backwards.

--- index.js
+++ index.js
@@ -2,22 +2,21 @@
 
 function main(alias) {
   const Now = require('now-client');
-  const now = Now(); // this looks like it detect the token from ~/.now.json or the env automatically
+  const now = Now(); // this detects the token from ~/.now.json or the env automatically
   const pkg = require(process.cwd() + '/package.json');
 
   return now.getDeployments().then(res => {
-    const [latest, prev, ...rest] = res.filter(_ => _.name === pkg.name).sort((a, b) => b.created - a.created);
+    const [latest, ...releases] = res.filter(_ => _.name === pkg.name).sort((a, b) => b.created - a.created);
 
-    if (!prev) {
+    if (!releases.length) {
       throw new Error('no previous build found to realias to');
     }
 
     if (alias) {
-      console.log(`aliasing to ${alias}...`);
+      console.log(`manually aliasing to ${alias}...`);
       return now.createAlias(latest.uid, alias);
     } else {
-      console.log(`getting alias for ${prev.uid} (${prev.url})`);
-      return now.getAliases(prev.uid).then(res => {
+      return getAliases(now, releases).then(res => {
         const first = res.shift(); // expecting a single alias - otherwise ¯\_(ツ)_/¯
         console.log(`aliasing to ${first.alias}...`);
         return now.createAlias(latest.uid, first.alias);
@@ -27,9 +26,28 @@ function main(alias) {
   });
 }
 
+function getAliases(now, array) {
+  const key = array[0];
+
+  if (!key) {
+    Promise.reject('No aliases found');
+  }
+
+  console.log(`getting alias for ${key.uid} (${key.url})`);
+
+  return now.getAliases(key.uid).then(res => {
+    if (res.length) {
+      console.log('none found, trying next.');
+      return res;
+    }
+
+    return getAliases(now, array.slice(1));
+  });
+}
+
+
 module.exports = main;
 
 if (!module.parent) {
-  let alias = process.argv.length > 1 && process.argv[process.argv.length - 1];
-  main(alias).then(() => console.log('done')).catch(e => console.error(e));
+  main(process.argv[2]).then(() => console.log('done')).catch(e => console.error(e));
 }
